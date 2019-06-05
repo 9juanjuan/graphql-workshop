185876e87918ada94f25b16a77f0017d3873b148
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-06-05 10:52:31 -0400

    creates root query, exports query and book object type, and imports to express server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const express = require ('express');
 const app = express(); 
 const PORT = 3000; 
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
 
 
 
