54f6f5f268b1117c38a7fd3d53ff744ed9ca76b0
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-06-05 10:22:57 -0400

    installs nodemon and writes start script to run nodemon

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require ('express'); 
 const app = express(); 
 const PORT = 3000; 
-const graphqlHTTP = requires('express-graphql');
+const graphqlHTTP = require('express-graphql');
 
 
 
